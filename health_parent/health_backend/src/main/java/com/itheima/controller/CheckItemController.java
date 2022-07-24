c5096da4fd270733bc3239ee79587a030ca13cd4
yepianer <aimeinvshen@126.com>
2022-07-24 20:00:33 +0800

    新增检查组 完善页面_弹出新增页面，发送ajax请求查询所有检查项数据，后台查询检查项数据，提交表单数据

--- health_parent/health_backend/src/main/java/com/itheima/controller/CheckItemController.java
+++ health_parent/health_backend/src/main/java/com/itheima/controller/CheckItemController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 /**
  * 检查项管理
  * */
@@ -70,9 +72,20 @@ public class CheckItemController {
     @RequestMapping("findById")
     public Result findById(Integer id){
         try {
-            System.out.println("123aaaaaaaaaaaaaaaaaa");
             CheckItem checkItem = checkItemService.findById(id);
             return new Result(true,MessageConstant.QUERY_CHECKITEM_SUCCESS,checkItem);
+        }catch (Exception e){
+            e.printStackTrace();
+            //服务调用失败
+            return new Result(false, MessageConstant.QUERY_CHECKITEM_FAIL);
+        }
+    }
+
+    @RequestMapping("findAll")
+    public Result findAll(){
+        try {
+            List<CheckItem> list = checkItemService.findAll();
+            return new Result(true,MessageConstant.QUERY_CHECKITEM_SUCCESS,list);
         }catch (Exception e){
             e.printStackTrace();
             //服务调用失败
--- health_parent/health_service_provider/src/main/java/com/itheima/dao/CheckItemDao.java
+++ health_parent/health_service_provider/src/main/java/com/itheima/dao/CheckItemDao.java
@@ -3,6 +3,8 @@ package com.itheima.dao;
 import com.github.pagehelper.Page;
 import com.itheima.pojo.CheckItem;
 
+import java.util.List;
+
 
 public interface CheckItemDao {
     public void add(CheckItem checkItem);
@@ -10,6 +12,6 @@ public interface CheckItemDao {
     public long findCountByCheckItemId(Integer id);
     public void deleteById(Integer id);
     public void edit(CheckItem checkItem);
-
     public CheckItem findById(Integer id);
+    public List<CheckItem> findAll();
 }
