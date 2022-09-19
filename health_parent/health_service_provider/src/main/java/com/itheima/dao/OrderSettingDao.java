84ee511e557e4b9d8f6f3f68414304e90b3d79bd
yepianer <aimeinvshen@126.com>
2022-09-19 18:05:25 +0800

    体检预约——后台代码 测试完成

--- health_parent/health_service_provider/src/main/java/com/itheima/dao/OrderSettingDao.java
+++ health_parent/health_service_provider/src/main/java/com/itheima/dao/OrderSettingDao.java
@@ -8,6 +8,7 @@ import java.util.List;
 public interface OrderSettingDao {
     public void add(OrderSetting orderSetting);
     public void editNumberByOrderDate(OrderSetting orderSetting);
+    public void editReservationsByOrderDate(OrderSetting orderSetting);
     public long findCountByOrderDate(Date orderDate);
     public List<OrderSetting> getOrderSettingByMonth(String date);
     public OrderSetting findByOrderDate(Date orderDate);
--- health_parent/health_service_provider/src/main/java/com/itheima/service/impl/OrderServiceImpl.java
+++ health_parent/health_service_provider/src/main/java/com/itheima/service/impl/OrderServiceImpl.java
@@ -2,14 +2,20 @@ package com.itheima.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.itheima.constant.MessageConstant;
+import com.itheima.dao.MemberDao;
+import com.itheima.dao.OrderDao;
 import com.itheima.dao.OrderSettingDao;
 import com.itheima.entity.Result;
+import com.itheima.pojo.Member;
+import com.itheima.pojo.Order;
 import com.itheima.pojo.OrderSetting;
 import com.itheima.service.OrderService;
 import com.itheima.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -21,12 +27,15 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private OrderSettingDao orderSettingDao;
-
+    @Autowired
+    private MemberDao memberDao;
+    @Autowired
+    private OrderDao orderDao;
     //体检预约
     @Override
     public Result order(Map map) throws Exception {
         //1.检查用户所选的预约日期是否已经提前进行了预约设置，如果没有设置则无法进行预约
-        String orderDate = (String)map.get("OrderDate");//预约日期
+        String orderDate = (String)map.get("orderDate");//预约日期
         OrderSetting orderSetting = orderSettingDao.findByOrderDate(DateUtils.parseString2Date(orderDate));
         if (orderSetting == null){
             //指定日期没有进行预约设置，无法完成预约
@@ -40,9 +49,42 @@ public class OrderServiceImpl implements OrderService {
             return new Result(false,MessageConstant.ORDER_FULL);
         }
         //3.检查用户是否重复预约（同一个用户同一天预约了同一个套餐），如果重复预约则无法完成预约
-
-        //4.检查当前用户是否为会员，如果是会员则直接完成预约，如果不是会员则自动完成注册并进行预约
+        String telephone = (String) map.get("telephone");//获取用户页面输入的手机号
+        Member member = memberDao.findByTelephone(telephone);
+        if (member != null){
+            //判断是否在重复预约
+            Integer memberId = member.getId();//会员ID
+            Date order_Date = DateUtils.parseString2Date(orderDate);//预约日期
+            String setmealId = (String) map.get("setmealId");//套餐ID
+            Order order = new Order(memberId,order_Date,Integer.parseInt(setmealId));
+            //根据条件进行查询
+            List<Order> list = orderDao.findByCondition(order);
+            if (list != null && list.size() > 0){
+                //说明用户在重复预约，无法完成再次预约
+                return new Result(false,MessageConstant.HAS_ORDERED);
+            }
+        }else {
+            //4.检查当前用户是否为会员，如果是会员则直接完成预约，如果不是会员则自动完成注册并进行预约
+            member = new Member();
+            member.setName((String) map.get("name"));
+            member.setPhoneNumber(telephone);
+            member.setIdCard((String) map.get("idCard"));
+            member.setSex((String) map.get("sex"));
+            member.setRegTime(new Date());
+            memberDao.add(member);//自动完成会员注册
+        }
         //5.预约成功，更新当日的已预约人数
-        return null;
+        Order order = new Order();
+        order.setMemberId(member.getId());//设置会员ID
+        order.setOrderDate(DateUtils.parseString2Date(orderDate));//设置预约日期
+        order.setOrderType((String) map.get("orderType"));//预约类型
+        order.setOrderStatus(Order.ORDERSTATUS_NO);//到诊状态
+        order.setSetmealId(Integer.parseInt((String) map.get("setmealId")));//设置套餐ID
+        orderDao.add(order);
+
+        orderSetting.setReservations(orderSetting.getReservations()+1);//设置已预约人数+1
+
+        orderSettingDao.editReservationsByOrderDate(orderSetting);
+        return new Result(true,MessageConstant.ORDER_SUCCESS,order.getId());
     }
 }
