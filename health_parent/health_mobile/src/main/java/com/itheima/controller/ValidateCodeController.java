d6a651f514cd90e49211c4bbbd2faa94b16a79d9
yepianer <aimeinvshen@126.com>
2022-09-21 10:29:27 +0800

    手机快速登录 发送验证码

--- health_parent/health_mobile/src/main/java/com/itheima/controller/ValidateCodeController.java
+++ health_parent/health_mobile/src/main/java/com/itheima/controller/ValidateCodeController.java
@@ -38,10 +38,11 @@ public class ValidateCodeController {
         return new Result(true,MessageConstant.SEND_VALIDATECODE_SUCCESS);
     }
 
+    //用户手机快速登录发送验证码
     @RequestMapping("/send4Login.do")
     public Result send4Login(String telephone){
-        //随机生成一个4位数字验证码
-        Integer validateCode = ValidateCodeUtils.generateValidateCode(4);
+        //随机生成一个6位数字验证码
+        Integer validateCode = ValidateCodeUtils.generateValidateCode(6);
         //给用户发送验证码
         //给用户发送验证码
         try {
@@ -50,8 +51,8 @@ public class ValidateCodeController {
             e.printStackTrace();
             return new Result(false, MessageConstant.SEND_VALIDATECODE_FAIL);
         }
-        //讲验证码保存到redis（5分钟）
-        jedisPool.getResource().setex(telephone + RedisMessageConstant.SENDTYPE_ORDER,600,validateCode.toString());
+        //讲验证码保存到redis（10分钟）
+        jedisPool.getResource().setex(telephone + RedisMessageConstant.SENDTYPE_LOGIN,600,validateCode.toString());
         return new Result(true,MessageConstant.SEND_VALIDATECODE_SUCCESS);
     }
 
