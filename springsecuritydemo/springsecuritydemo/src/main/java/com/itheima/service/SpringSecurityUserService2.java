cc1e54f03ed7b46e7f39f1b20fb0d3d1385d693e
yepianer <aimeinvshen@126.com>
2022-11-24 15:06:59 +0800

    权限控制 对入门案例进行的改进 配置多种校验规则

--- springsecuritydemo/springsecuritydemo/src/main/java/com/itheima/service/SpringSecurityUserService2.java
+++ springsecuritydemo/springsecuritydemo/src/main/java/com/itheima/service/SpringSecurityUserService2.java
@@ -1,34 +1,42 @@
 package com.itheima.service;
 
 import com.itheima.pojo.User;
-import com.mysql.cj.x.protobuf.MysqlxDatatypes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SpringSecurityUserService implements UserDetailsService {
+public class SpringSecurityUserService2 implements UserDetailsService {
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
     //模拟数据库中用户数据
-    public static Map<String, User> map = new HashMap<>();
-    static {
+    public Map<String, User> map = new HashMap<>();
+
+    public void initUserData(){
         User user1 = new User();
         user1.setUsername("admin");
-        user1.setPassword("admin");//明文密码，没有加密
+        user1.setPassword(passwordEncoder.encode("admin"));//使用bcrypt提供的方法对密码进行加密
 
         User user2 = new User();
         user2.setUsername("xiaoming");
-        user2.setPassword("1234");
+        user2.setPassword(passwordEncoder.encode("1234"));
 
         map.put(user1.getUsername(), user1);
         map.put(user2.getUsername(),user2);
     }
+
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        initUserData();
+
         System.out.println("用户输入的用户名为："+ username);
         //根据用户名查询数据库获得用户信息，包含数据库中存储的密码信息
         User user = map.get(username);
@@ -44,9 +52,10 @@ public class SpringSecurityUserService implements UserDetailsService {
             list.add(new SimpleGrantedAuthority("permission_B"));
             if (username.equals("admin")){
                 list.add(new SimpleGrantedAuthority("ROLE_ADMIN"));//授予角色
+                list.add(new SimpleGrantedAuthority("add"));//授权
             }
             org.springframework.security.core.userdetails.User securityUser
-                    = new org.springframework.security.core.userdetails.User(username,"{noop}"+user.getPassword(),list);
+                    = new org.springframework.security.core.userdetails.User(username,user.getPassword(),list);
             return  securityUser;
         }
 
