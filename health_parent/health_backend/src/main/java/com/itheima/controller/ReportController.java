87cf75db38d1f3a1acc5a8300594e6cefd12ad3a
yepianer <aimeinvshen@126.com>
2022-12-15 11:08:27 +0800

    会员数量折线图_后台代码 Controller动态计算月份数据

--- health_parent/health_backend/src/main/java/com/itheima/controller/ReportController.java
+++ health_parent/health_backend/src/main/java/com/itheima/controller/ReportController.java
@@ -7,10 +7,8 @@ import com.itheima.service.MemberService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * 报表操作
@@ -25,25 +23,22 @@ public class ReportController {
     //会员数量折线图数据
     @RequestMapping("/getMemberReport")
     public Result getMemberReport(){
-        //使用模拟数据测试对象格式是否能够转为echarts所需数据格式
         Map<String,Object> map = new HashMap<>();
         List<String> months = new ArrayList<>();
-        months.add("2018.05");
-        months.add("2018.06");
-        months.add("2018.07");
-        months.add("2018.08");
-        months.add("2018.09");
+        Calendar calendar = Calendar.getInstance();//获得日历对象。模拟时间就是当前时间
+        //计算过去一年12个月
+        calendar.add(Calendar.MONDAY,-12);//获得当前时间往前推12个月
+        for (int i=0;i<12;i++){
+            calendar.add(Calendar.MONDAY,1);//获得当前时间往后推一个月
+            Date date = calendar.getTime();
+            months.add(new SimpleDateFormat("yyyy,MM").format(date));
+        }
         map.put("months",months);
 
-        List<Integer> memberCount = new ArrayList<>();
-        memberCount.add(100);
-        memberCount.add(150);
-        memberCount.add(180);
-        memberCount.add(200);
-        memberCount.add(300);
-
+        List<Integer> memberCount = memberService.findMemberCountByMonths(months);
         map.put("memberCount",memberCount);
-
         return new Result(true, MessageConstant.GET_MEMBER_NUMBER_REPORT_SUCCESS,map);
     }
+
+
 }
