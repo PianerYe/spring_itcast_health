d0e3156783cab9e3f467bb5019acb7621120ba7e
yepianer <aimeinvshen@126.com>
2022-12-20 19:03:59 +0800

    运营数据统计-后台代码 服务实现类查询会员数据和预约到诊数据

--- health_parent/health_interface/src/main/java/com/itheima/service/ReportService.java
+++ health_parent/health_interface/src/main/java/com/itheima/service/ReportService.java
@@ -3,5 +3,5 @@ package com.itheima.service;
 import java.util.Map;
 
 public interface ReportService {
-    public Map<String,Object> getBusinessReportData();
+    public Map<String,Object> getBusinessReportData() throws Exception;
 }
---
+++ health_parent/health_service_provider/src/main/java/com/itheima/service/impl/ReportServiceImpl.java
@@ -0,0 +1,72 @@
+package com.itheima.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.itheima.dao.MemberDao;
+import com.itheima.dao.OrderDao;
+import com.itheima.service.ReportService;
+import com.itheima.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 运营数据统计服务
+ * */
+@Service(interfaceClass = ReportService.class)
+@Transactional
+public class ReportServiceImpl implements ReportService {
+        @Autowired
+        private MemberDao memberDao;
+        @Autowired
+        private OrderDao orderDao;
+    @Override
+    //查询运营数据
+    public Map<String, Object> getBusinessReportData() throws Exception {
+        Map<String,Object> map = new HashMap<>();
+        //报表日期
+        String today = DateUtils.parseDate2String(DateUtils.getToday());
+        //获取本周一日期
+        String monday = DateUtils.parseDate2String(DateUtils.getThisWeekMonday());
+        //获得本月第一天日期
+        String firstDay4ThisMonth = DateUtils.parseDate2String(DateUtils.getFirstDay4ThisMonth());
+
+        //本日新增会员数
+        Integer todayNewMember = memberDao.findMemberCountByDate(today);
+        //总会员数
+        Integer totalMember = memberDao.findMemberTotalCount();
+        //本周新增会员数
+        Integer thisWeekNewMember = memberDao.findMemberCountAfterDate(monday);
+        //本月新增会员数
+        Integer thisMonthNewMember = memberDao.findMemberCountAfterDate(firstDay4ThisMonth);
+        //今日预约数
+        Integer todayOrderNumber = orderDao.findOrderCountByDate(today);
+        //今日到诊数
+        Integer toderVisitsNumber = orderDao.findVisitsCountByDate(today);
+        //本周预约数
+        Integer thisWeekOrderNumber = orderDao.findOrderCountAfterDate(monday);
+        //本周到诊数
+        Integer thisWeekVisitsNumber = orderDao.findVisitsCountAfterDate(monday);
+        //本月预约数
+        Integer thisMonthOrderNumber = orderDao.findOrderCountAfterDate(firstDay4ThisMonth);
+        //本月到诊数
+        Integer thisMonthVisitsNumber = orderDao.findVisitsCountAfterDate(firstDay4ThisMonth);
+
+        //日期
+        map.put("reportDate",today);
+        map.put("todayNewMember",todayNewMember);
+        map.put("totalMember",totalMember);
+        map.put("thisWeekNewMember",thisWeekNewMember);
+        map.put("thisMonthNewMember",thisMonthNewMember);
+        map.put("todayOrderNumber",todayOrderNumber);
+        map.put("toderVisitsNumber",toderVisitsNumber);
+        map.put("thisWeekOrderNumber",thisWeekOrderNumber);
+        map.put("thisWeekVisitsNumber",thisWeekVisitsNumber);
+        map.put("thisMonthOrderNumber",thisMonthOrderNumber);
+        map.put("thisMonthVisitsNumber",thisMonthVisitsNumber);
+
+        return map;
+    }
+}
